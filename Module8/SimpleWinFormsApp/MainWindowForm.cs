c5a9d5c4af5f1dba7063a54033b3f680a3e604a8
Sean McIlvenny <smcilvenny22@gmail.com>
2019-11-14 15:01:49 -0800

    Class Demo/Labs for Thursday 11/14 I got a little behind on the first one

---
+++ Module8/SimpleWinFormsApp/MainWindowForm.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SimpleWinFormsApp
+{
+    public partial class MainWindowForm : Form
+    {
+        public MainWindowForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindowForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
